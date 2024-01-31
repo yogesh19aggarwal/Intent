fa3b2cc56abe0788b2fdcdc21db655312d08ccfd
Yogesh Aggarwal <yogesh.19.aggarwal@gmail.com>
2024-01-31 19:20:43 +0530

    adding files

---
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.layout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnIntent = findViewById<Button>(R.id.click)
+
+        btnIntent.setOnClickListener {
+            // open a new activity
+
+            intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
